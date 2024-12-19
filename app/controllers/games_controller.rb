5e835b697d3efa7ce5c9561ef95914c5a419747d
nico alavi <alavinic@gmail.com>
2024-12-19 19:39:19 +0900

    set up a standard array of letters.

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,8 @@
+class GamesController < ApplicationController
+  def new
+    @letters = %w[Y Z D U Q E Z Y Q I]
+  end
+
+  def score
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  # commenting out these lines that were automatically generated
+  # get 'games/new'
+  # get 'games/score'
+  get 'new', to: 'games#new'
+  post 'score', to: 'games#score'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
